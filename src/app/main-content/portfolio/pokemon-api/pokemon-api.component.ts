0978749f0aeab922111e89ab4707cc5d0916b22f
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-09-30 20:16:17 +0200

    add join- pollp-loco- pokemonAPI Component

---
+++ src/app/main-content/portfolio/pokemon-api/pokemon-api.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-api',
+  standalone: true,
+  imports: [],
+  templateUrl: './pokemon-api.component.html',
+  styleUrl: './pokemon-api.component.scss'
+})
+export class PokemonAPIComponent {
+
+}
