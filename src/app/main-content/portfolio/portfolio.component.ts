f302fbe6e95e6aa4ae3f3f9c5b2d3d90d8b55365
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-10-01 09:22:40 +0200

    add css Jon & Pollo Loco

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { JoinComponent } from "./join/join.component";
+import { PolloLocoComponent } from "./pollo-loco/pollo-loco.component";
+import { PokemonAPIComponent } from "./pokemon-api/pokemon-api.component";
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [JoinComponent, PolloLocoComponent, PokemonAPIComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
