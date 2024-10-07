18f5e674ada1b7aa2c9702d5d27f67216bf225fb
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-10-07 16:59:12 +0200

    mail contakt

--- src/app/main-content/kontakt/kontakt.component.ts
+++ src/app/main-content/kontakt/kontakt.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-kontakt',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './kontakt.component.html',
   styleUrl: './kontakt.component.scss'
 })
 export class KontaktComponent {
 
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(ngForm.value);
+    }
+  }
 }
