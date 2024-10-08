a361560ad482b2a94d14c678428b4f93ef4aff77
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-10-08 12:37:34 +0200

    Mail test

--- src/app/main-content/kontakt/kontakt.component.ts
+++ src/app/main-content/kontakt/kontakt.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
@@ -10,15 +11,43 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class KontaktComponent {
 
-  contactData = {
+  http = inject(HttpClient);
+
+  contactData =  {
     name: '',
     email: '',
     message: '',
   }
 
+  mailTest = false ; // nach den Testen der Mailfunktion auf false setzen
+
+  post = {
+    endPoint: 'https://ingo-kasper.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(ngForm.value);
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            // Platz um zu erweitern der Funktionen
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {  // der else if wird für das Testen der Mailfunktion benötigt
+      // Platz um zu erweitern der Funktionen
+      ngForm.resetForm();
     }
   }
 }
