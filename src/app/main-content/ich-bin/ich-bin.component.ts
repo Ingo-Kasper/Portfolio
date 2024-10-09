ed022bf6f9ee2d522b1e8e7795acee91baceb3b5
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-10-09 20:29:37 +0200

    Überstzung für ich-bin.component & uber-mich.component

--- src/app/main-content/ich-bin/ich-bin.component.ts
+++ src/app/main-content/ich-bin/ich-bin.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../services/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-ich-bin',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './ich-bin.component.html',
   styleUrl: './ich-bin.component.scss'
 })
 export class IchBinComponent {
 
+  translate = inject(TranslationService)
+  
 }
--- src/app/main-content/uber-mich/uber-mich.component.ts
+++ src/app/main-content/uber-mich/uber-mich.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../../services/translation.service';
 
 @Component({
   selector: 'app-uber-mich',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './uber-mich.component.html',
   styleUrl: './uber-mich.component.scss'
 })
 export class UberMichComponent {
 
+  translate = inject(TranslationService)
 }
