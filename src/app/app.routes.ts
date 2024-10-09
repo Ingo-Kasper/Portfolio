0b71feb61400d2b8b9df14f39a1d771d3e51cbfc
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-10-09 13:49:07 +0200

    add Translation Funkcion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,8 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { DatenschutzComponent } from './datenschutz/datenschutz.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 
-
 export const routes: Routes = [
-    {path: '', component: MainContentComponent },
-    {path: 'datenschutz', component: DatenschutzComponent },
-    {path: 'impressum', component: ImpressumComponent}
+  { path: '', component: MainContentComponent },
+  { path: 'datenschutz', component: DatenschutzComponent },
+  { path: 'impressum', component: ImpressumComponent },
 ];
--- src/app/main-content/kontakt/kontakt.component.ts
+++ src/app/main-content/kontakt/kontakt.component.ts
@@ -26,8 +26,8 @@ export class KontaktComponent {
     body: (payload: any) => JSON.stringify(payload),
     options: {
       headers: {
-        'Content-Type': 'text/plain',
-        responseType: 'text',
+        'Content-Type': 'application/json',
+        responseType: 'text' as 'json',
       },
     },
   };
