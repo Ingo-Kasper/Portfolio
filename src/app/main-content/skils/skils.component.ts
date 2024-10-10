65e482cc424a6283bc3268e2d028353a8640f8f8
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-10-10 09:52:11 +0200

    Übersetung, skill.component

--- src/app/main-content/skils/skils.component.ts
+++ src/app/main-content/skils/skils.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SoloSkilComponent } from "./solo-skil/solo-skil.component";
+import { TranslationService } from '../../services/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skils',
   standalone: true,
-  imports: [SoloSkilComponent],
+  imports: [SoloSkilComponent, TranslateModule],
   templateUrl: './skils.component.html',
   styleUrl: './skils.component.scss'
 })
 export class SkilsComponent {
+
+  translate = inject(TranslationService)
 
 }
