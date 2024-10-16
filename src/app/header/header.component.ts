747a5a86e890275efc9e84b40e2df2ecab669497
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-10-16 11:01:29 +0200

    Header, burger-Menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../services/translation.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
   translate = inject(TranslationService);
+
+  isNavOpen = false;
+
+  toggleNav() {
+    this.isNavOpen = !this.isNavOpen;
+  }
 
 }
